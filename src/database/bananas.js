fb47005f146bb7a14443060617c12c9fd4d6acd1
serhan <serhanyavuz5@gmail.com>
2022-02-03 11:25:04 +0300

    init

---
+++ src/database/bananas.js
@@ -0,0 +1,45 @@
+const {getdatabase} = require('./mongo');
+const { ObjectId} = require('mongodb');
+
+const collectionName = 'bananas';
+
+async function insertBanana(banana){
+    const database = await getdatabase();
+    const{insertedId} = await database.collection(collectionName).insertOne(banana);
+    return insertedId;
+}
+async function getAllBananas(){
+    const database = await getdatabase();
+    return await database.collection(collectionName).find({}).toArray();
+}
+async function getBanana(id){
+    const database = await getdatabase();
+    try{
+        var oid = new ObjectId(id);
+        return await database.collection(collectionName).findOne({_id: oid});
+    }catch(e){
+        return({message : 'öyle bi muz bulunamadı'});
+    }
+    
+}
+async function updateBanana(id, banana ){
+    const database = await getdatabase();
+    delete banana._id;
+    try{
+        await database.collection(collectionName).update(
+            {_id: new ObjectId(id),},
+            {
+                $set:{...banana,},
+            },
+        );
+    }catch(e){
+        return({message: 'güncellenemedi !'})
+    }
+
+}
+module.exports = {
+    insertBanana,
+    updateBanana,
+    getBanana,
+    getAllBananas,
+};
---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+const {startDatabase} = require('./database/mongo');
+const {insertBanana, getAllBananas,getBanana, updateBanana} = require('./database/bananas');
+
+const app = express();
+
+
+app.use(helmet());
+app.use(bodyParser.json())
+app.use(cors());
+app.use(morgan('combined'));
+
+
+app.get('/', async (req,res) => {
+    res.send( await getAllBananas() );
+});
+
+const checkJwt = jwt(
+    {
+        secret:jwksRsa.expressJwtSecret({
+            cache : true,
+            rateLimit : true,
+            jwksRequestsPerMinute: 20,
+            jwksUri: 'http://161.35.159.65:8081/.well-known/openid-configuration/jwks'
+        }),
+        audience: 'potatoApi',
+        issuer: 'http://161.35.159.65:8081',
+        algorithms: ['RS256']
+    }
+);
+app.use(checkJwt);
+
+app.get('/:id', async (req,res) => {
+    res.send( await getBanana(req.params.id) );
+});
+
+app.post('/', async (req,res) => {
+    const newBanana = req.body;
+    newBanana.isDeleted = false;
+    await insertBanana(newBanana);
+    res.send({message: 'Yeni muzunuz kaydedildi'});
+});
+
+//TODO güncelleme işlemi hata versede sonuç doğru gibi oluyor !
+app.put('/:id', async(req,res) => {
+    const bananaToBeUpdated = req.body;
+    await updateBanana(req.params.id, bananaToBeUpdated);
+    res.send({message: 'muzunuz güncellendi.'});
+});
+
+app.delete('/:id', async(req,res) => {
+    var bananaToBeDeleted = await getBanana(req.params.id);
+    if(bananaToBeDeleted == null || bananaToBeDeleted== undefined){
+        res.send({message: 'öyle bi muz bulunamadı'})
+    }else{
+        bananaToBeDeleted.isDeleted = true
+        res.send({message: 'muzunuz silindi!'});
+    }
+
+});
+
+startDatabase().then(async () => {
+    console.log('Initializing db');
+    await insertBanana({name: "sait faik", origin: "ankara", owner: "245669", isDeleted:false});
+    console.log('listening on port 3001');
+});
+
+app.listen(3001, () => {
+    console.log('starting app');
+});
